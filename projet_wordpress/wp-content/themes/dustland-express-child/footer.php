a8718337ed9c7556d88f8f6f5ed618106e307e56
qml95 <quentin.morino.laudrin@gmail.com>
2017-12-29 17:52:33 +0100

    add new footer mention legales and cgv. add new css for color footer and button

--- projet_wordpress/wp-content/themes/dustland-express-child/footer.php
+++ projet_wordpress/wp-content/themes/dustland-express-child/footer.php
@@ -25,11 +25,6 @@
             <div class="clearboth"></div>
         </div>
     </div>
-	
-		<?php printf( __( '<div class="site-footer-bottom-bar"><div class="site-container"><div class="site-footer-bottom-bar-left">Theme: %1$s by %2$s', 'dustland-express' ), 'Dustland Express', '<a href="https://www.kairaweb.com/">Kaira</a></div><div class="site-footer-bottom-bar-right">' ); ?>
-
-	            <?php wp_nav_menu( array( 'theme_location' => 'footer-bar','container' => false, 'fallback_cb' => false, 'depth'  => 1 ) ); ?>
-
 	        </div>
 
 	    </div>
